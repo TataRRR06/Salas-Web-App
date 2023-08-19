77c2f7dc64a462d9d9aa274b0e0b460cb8790b3c
TataRRR06 <111392932+TataRRR06@users.noreply.github.com>
2023-08-18 21:18:38 -0600

    Add files via upload

--- Salas_page.js
+++ Salas_page.js
@@ -1,24 +1,25 @@
 //TUS ANLACES DE FIREBASE
 
 var firebaseConfig = {
-      apiKey: "AIzaSyBIQWLr1yRTFXod2qS0lAboN1poEdvIqtI",
-      authDomain: "salas-web-app.firebaseapp.com",
-      databaseURL: "https://salas-web-app-default-rtdb.firebaseio.com",
-      projectId: "salas-web-app",
-      storageBucket: "salas-web-app.appspot.com",
-      messagingSenderId: "409528221421",
-      appId: "1:409528221421:web:981d87d07317c65e0816e4"
+      apiKey: "AIzaSyBkoQfdU2HkCxl8JaAP1B42I9GbvOxhHL0",
+      authDomain: "salas-web-app-p93-97.firebaseapp.com",
+      databaseURL: "https://salas-web-app-p93-97-default-rtdb.firebaseio.com",
+      projectId: "salas-web-app-p93-97",
+      storageBucket: "salas-web-app-p93-97.appspot.com",
+      messagingSenderId: "552378182231",
+      appId: "1:552378182231:web:f46761a3b92b1990c49112"
     };
+    
     
     firebase.initializeApp(firebaseConfig);
 
-var user_name = localStorage.getItem("user_name");
-var room_name = localStorage.getItem("room_name");
+user_name = localStorage.getItem("user_name");
+      room_name = localStorage.getItem("room_name");
 
-document.getElementById("output").innerHTML="Hola,"+ user_name+"bienvenid@ de nuevo a tu Sala "+ room_name+"";
+document.getElementById("user_name").innerHTML="Hola,"+ user_name+"bienvenid@ de nuevo a tu Sala "+ room_name+""; //elemento id
 
 function send(){
-msg.getElementById("msg").value;
+msg=document.getElementById("msg").value;
 firebase.database().ref(room_name).push({
 name:user_name,
 message:msg,
@@ -26,23 +27,33 @@ like:0
 }); 
 document.getElementById("msg").value="";
 }
-function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
-         firebase_message_id = childKey;
-         message_data = childData;
-//Inica código
-console.log(firebase_message_id);
-console.log(message_data);
-name=message_data['name'];
-message=message_data['message'];
-like=message_data['like'];
-name_with_tag="<h4>"+name+"<img class='user_tick' src='tick.png'></h4>";
-message_with_tag="<h4 class='message_h4'>"+message+"</h4>";
-like_button="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+"onclick='updateLike(this.id)'> "
-span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: "+like +"</span></button><hr>";
-row= name_with_tag + message_with_tag +like_button + span_with_tag;
-document.getElementById("output").innerHTML += row;
-//Termina código
-      } });  }); }
+function getData() { 
+      firebase.database().ref("/"+room_name).on('value', function(snapshot) { 
+          document.getElementById("output").innerHTML = "";
+          snapshot.forEach(function(childSnapshot) { 
+              childKey = childSnapshot.key;
+              childData = childSnapshot.val();
+               if(childKey != "purpose") {
+          firebase_message_id = childKey;
+          message_data = childData;
+            //Inica código
+            console.log(firebase_message_id);
+            console.log(message_data);
+            name = message_data['name'];
+            message = message_data['message'];
+            like = message_data['like'];
+            name_with_tag = "<h4> "+ name + "<img class='user_tick' src='tick.png'></h4>";
+            message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
+            like_button = "<button class='btn btn-warning' id=" + firebase_message_id + " value=" + like + " onclick='updateLike(this.id)'>";
+            span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: " + like + "</span></button><hr>";
+            row = name_with_tag + message_with_tag + like_button + span_with_tag;
+            document.getElementById("output").innerHTML += row;
+            //Termina código
+      
+      }
+ }); 
+ }); 
+}
 getData();
 
 
@@ -51,13 +62,16 @@ function updateLike (message_id)
 console.log("Botón Me gusta pulsado -" + message_id);
 button_id = message_id;
 likes = document.getElementById(button_id).value;
-updated_likes=Number (likes) + 1;
+updated_likes=Number(likes) + 1;
 console.log(updated_likes);
-firebase.database().ref(room_name).child(message_id).update({like: updated_likes});
+
+firebase.database().ref(room_name).child(message_id).update({
+      like: updated_likes
+});
 }
 
 function logout(){
       localStorage.removeItem("user_name");
       localStorage.removeItem("room_name");
-      window.location="kwitter.html";
+      window.location="index.html";
 }
--- Salas_room.js
+++ Salas_room.js
@@ -1,46 +1,57 @@
 
 //AÑADE TUS ENLACES DE FIREBASE
-
-
 var firebaseConfig = {
-  apiKey: "AIzaSyBIQWLr1yRTFXod2qS0lAboN1poEdvIqtI",
-  authDomain: "salas-web-app.firebaseapp.com",
-  databaseURL: "https://salas-web-app-default-rtdb.firebaseio.com",
-  projectId: "salas-web-app",
-  storageBucket: "salas-web-app.appspot.com",
-  messagingSenderId: "409528221421",
-  appId: "1:409528221421:web:981d87d07317c65e0816e4"
-};
+      apiKey: "AIzaSyBkoQfdU2HkCxl8JaAP1B42I9GbvOxhHL0",
+      authDomain: "salas-web-app-p93-97.firebaseapp.com",
+      databaseURL: "https://salas-web-app-p93-97-default-rtdb.firebaseio.com",
+      projectId: "salas-web-app-p93-97",
+      storageBucket: "salas-web-app-p93-97.appspot.com",
+      messagingSenderId: "552378182231",
+      appId: "1:552378182231:web:f46761a3b92b1990c49112"
+    };
+    
+    firebase.initializeApp(firebaseConfig);
 
-firebase.initializeApp(firebaseConfig);
-
-var user_name = localStorage.getItem("user_name");
-  
-var room_name = localStorage.getItem("room_name");
+user_name = localStorage.getItem("user_name");  
+room_name = localStorage.getItem("room_name");
 
 document.getElementById("user_name").innerHTML="Hola, Bienvenid@ " + user_name;
 
 function addRoom()
 {
 room_name=document.getElementById("room_name").value;
-firebase.database().ref("/").child(room_name).update({purpose:"adding room name"
+firebase.database().ref("/").child(room_name).update({
+      purpose:"adding room name"
 });
 
 localStorage.setItem("room_name", room_name);
 
-window.location="Salas_page.html";
+window.location.replace("Salas_page.html");
 }
 
 
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-       Room_names = childKey;
-      //Inicio del código
-console.log("room_name - "+ Room_names);
-row="<div class='room_name' id="+ Room_names+ "onclick='redirectToRoomName(this.id)'> # "+ Room_names+ " </div><hr>";
-document.getElementById("output").innerHTML+=row;
-      //Final del código
-      });});}
+function getData() {
+      firebase.database().ref("/").on('value', function (snapshot) {
+            document.getElementById("output").innerHTML = "";
+            snapshot.forEach(function (childSnapshot) {
+                  childKey = childSnapshot.key;
+                  Room_names = childKey;
+            //Inicio del código
+            console.log("room_name - " + Room_names);
+            row = "<div class='room_name' id="+ Room_names +" onclick='redirectToRoomName(this.id)'>#" + Room_names + "</div><hr>";
+            document.getElementById("output").innerHTML += row;
+            //Final del código
+      });
+});
+}
 getData();
+
+
+function redirectToRoomName(name){
+console.log(name);
+localStorage.setItem("room_name", name);
+window.location="Salas_page.html";
+}
 
 
 function logout(){
